519380be765098149ec5261c3fa4e9d4232747b2
pivot5 <anshsharma10@yahoo.ca>
2020-09-21 14:45:32 -0400

    add travis and generic python code

---
+++ test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Test
+"""
+
+
+
+def main():
+    print("hello world")
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
